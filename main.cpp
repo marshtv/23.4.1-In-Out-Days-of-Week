09d00f1b77dd7a04463f3ef0116eb7c6fb79fd85
marshtv <marshtv.1972@gmail.com>
2022-12-29 20:31:50 +0300

    Complete++

--- main.cpp
+++ main.cpp
@@ -1,45 +1,42 @@
 #include <iostream>
 
 #define MON 1
-#define MONDAY "Monday"
 #define TUE 2
-#define TUESDAY "Tuesday"
 #define WED 3
-#define WEDNESDAY "Wednesday"
 #define THU 4
-#define THURSDAY "Thursday"
 #define FRI 5
-#define FRIDAY "Friday"
 #define SAT 6
-#define SATURDAY "Saturday"
 #define SUN 7
-#define SUNDAY "Sunday"
 
-int main() {
-	int day_week;
-	std::cout << "Input number of week's day:" << std::endl;
-	std::cin >> day_week;
-	switch (day_week) {
+void display_day_week(int x) {
+	switch (x) {
 		case MON:
-			std::cout << MONDAY << std::endl;
+			std::cout << "MONDAY" << std::endl;
 			break;
 		case TUE:
-			std::cout << TUESDAY << std::endl;
+			std::cout << "TUESDAY" << std::endl;
 			break;
 		case WED:
-			std::cout << WEDNESDAY << std::endl;
+			std::cout << "WEDNESDAY" << std::endl;
 			break;
 		case THU:
-			std::cout << THURSDAY << std::endl;
+			std::cout << "THURSDAY" << std::endl;
 			break;
 		case FRI:
-			std::cout << FRIDAY << std::endl;
+			std::cout << "FRIDAY" << std::endl;
 			break;
 		case SAT:
-			std::cout << SATURDAY << std::endl;
+			std::cout << "SATURDAY" << std::endl;
 			break;
 		case SUN:
-			std::cout << SUNDAY << std::endl;
+			std::cout << "SUNDAY" << std::endl;
 			break;
 	}
+}
+
+int main() {
+	int day_num;
+	std::cout << "Input number of week's day:" << std::endl;
+	std::cin >> day_num;
+	display_day_week(day_num);
 }
